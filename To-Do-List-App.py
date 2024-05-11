f854f5a49396f63a4ff197a23e86a989f5f580cf
Kisshor321 <154450989+Kisshor321@users.noreply.github.com>
2024-05-11 15:33:06 +0530

    To-Do-List-App.py

--- To-Do-List-App.py
+++ To-Do-List-App.py
@@ -27,7 +27,7 @@ root.geometry("410x650+400+100")
 root.resizable(False,False)
 
 
-text=Label(root,text="ADD LIST",width=30,font=("Arial",25,"bold"),bg="#606060",relief=SUNKEN)
+text=Label(root,text="📓  ADD LIST",width=30,font=("Arial",25,"bold"),bg="gray",relief=SUNKEN,fg="white")
 text.pack()
 
 entry=Entry(root,font=("arial",15),width=28,bd=5,bg="#93ff75",relief=SUNKEN)
@@ -50,14 +50,14 @@ listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 
 
-button1=Button(text="Clear The File",bg="#6970c2",command=remove,bd=5,relief=RAISED,width=27,font=("arial",12,"bold"))
+button1=Button(text="❎  Clear The File",bg="#6970c2",command=remove,bd=5,relief=RAISED,width=27,font=("arial",12,"bold"))
 button1.place(x=60,y=490)
 
-button1=Button(text="Mark As Read",bg="sky blue",command=mark,bd=5,relief=RAISED,width=27,font=("arial",12,"bold"))
+button1=Button(text="☑️  Mark As Read",bg="sky blue",command=mark,bd=5,relief=RAISED,width=27,font=("arial",12,"bold"))
 button1.place(x=60,y=540)
 
 
-button1=Button(text="Delete All Tasks",bg="yellow",command=delete,bd=5,relief=RAISED,width=27,font=("arial",12,"bold"))
-button1.place(x=60,y=590)
+label=Label(text="Thank you...🥰",bd=5,width=27,font=("arial",18,"bold"))
+label.place(x=10,y=590)
 
 root.mainloop()
